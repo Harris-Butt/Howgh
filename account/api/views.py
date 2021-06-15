155914f5432403a5b00a38a4de95d727da772d2e
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2021-06-15 12:29:20 +0500

    Add account registration

---
+++ account/api/views.py
@@ -0,0 +1,54 @@
+from account.api.serializers import RegistrationSerializer
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from account.models import Account
+
+
+@api_view(['POST', ])
+@permission_classes([])
+@authentication_classes([])
+def registration_view(request):
+
+	if request.method == 'POST':
+		data = {}
+		email = request.data.get('email', '0').lower()
+		if validate_email(email) != None:
+			data['error_message'] = 'That email is already in use.'
+			data['response'] = 'Error'
+			return Response(data)
+
+		username = request.data.get('username', '0')
+		if validate_username(username) != None:
+			data['error_message'] = 'That username is already in use.'
+			data['response'] = 'Error'
+			return Response(data)
+
+		serializer = RegistrationSerializer(data=request.data)
+		
+		if serializer.is_valid():
+			account = serializer.save()
+			data['response'] = 'successfully registered new user.'
+			data['email'] = account.email
+			data['username'] = account.username
+			data['pk'] = account.pk
+		else:
+			data = serializer.errors
+		return Response(data)
+
+def validate_email(email):
+    account = None
+    try:
+        account = Account.objects.get(email=email)
+    except Account.DoesNotExist:
+        return None
+    if account != None:
+        return email
+
+def validate_username(username):
+	account = None
+	try:
+		account = Account.objects.get(username=username)
+	except Account.DoesNotExist:
+		return None
+	if account != None:
+		return username
