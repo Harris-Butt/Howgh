35456dd24763999520fd21c787b18b706fd3275f
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2021-06-15 14:41:28 +0500

    Added files and changes required for deployment to heroku

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['howgh.herokuapp.com/','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['howgh.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
