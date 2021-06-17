bf50c752c56769e911dbb0a7ef99e11760863213
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2021-06-17 18:15:15 +0500

    add basic login

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from account.api.views import registration_view
+from account.api.views import registration_view, login_view
 
 
 
 urlpatterns = [
 	path('register', registration_view, name="register"),
+	path('login',login_view,name="login"),
 	]
--- account/api/views.py
+++ account/api/views.py
@@ -1,7 +1,9 @@
 from account.api.serializers import RegistrationSerializer
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
+from django.contrib.auth.hashers import check_password
 from account.models import Account
+
 
 
 
@@ -40,7 +42,7 @@ def registration_view(request):
 		return Response(data)
 
 def validate_email(email):
-	account = None
+	print("validate_email")
 	try:
 		account = Account.objects.get(email=email)
 	except Account.DoesNotExist:
@@ -56,3 +58,43 @@ def validate_username(username):
 		return None
 	if account != None:
 		return username
+
+def validate_password(user_email,user_password):
+	account = None
+	account = Account.objects.get(email=user_email)
+	print(f'printing account in validate password {account}')
+	account_password = check_password(user_password,account.password)
+	if account_password == False:
+		account = None
+	if account != None:
+		print(user_password)
+		return user_password
+
+@api_view(['POST', ])
+def login_view(request):
+	if request.method == 'POST':
+		authenticated_email = None
+		authenticated_password = None
+		data = {}
+		email = request.data.get('email', '0').lower()
+		print(email)
+		authenticated_email = validate_email(email)
+		print(authenticated_email)
+		if  authenticated_email == None:
+			data['error_message'] = 'Email or password does not match'
+			data['error'] = "true"
+			return Response(data)
+		user_password = request.data.get('password', '0')
+		authenticated_password = validate_password(authenticated_email,user_password)
+		if authenticated_password == None:
+			data['error_message'] = 'Email or password does not match'
+			data['error'] = "true"
+			return Response(data)
+
+		if authenticated_email != None and authenticated_password != None:
+			data["success_message"] = "user successfully login"
+			data['error'] = "false"
+			return Response(data)
+		return Response(data)
+
+
